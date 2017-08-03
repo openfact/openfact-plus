79456ba4167e499cd7989079d03e6b223a17acc1
Carlos Feria <carlosthe19916@gmail.com>
2017-08-03 17:39:17 -0500

    add elasticsearch

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ mvn fabric8:deploy -DSSO_REALM=openfact \
     -DSSO_AUTH_SERVER_URL=http://`oc get route keycloak --template={{.spec.host}}`/auth \
     -DSSO_CLIENT_ID=openfact-online-platform \
     -DSSO_CREDENTIALS_SECRET=b7873f80-6ad7-44a5-97cb-bd210789eb41 \
+    -DES_SERVER_URL=http://192.168.1.30:9200 \
+    -DES_USERNAME=elastic \
+    -DES_PASSWORD=changeme \
     -Popenshift \
     -DskipTests=true \
     -Dfabric8.debug.enabled=true
