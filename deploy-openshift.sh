4598972c1218b764f36866992b9ab5facf986d8c
Carlos Feria <carlosthe19916@gmail.com>
2017-10-10 12:17:38 -0500

    fled(elasticsearch): config elasticsearch on wildfly.swarm

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -10,6 +10,9 @@ mvn fabric8:deploy -DSSO_REALM=openfact \
     -DSSO_AUTH_SERVER_URL=http://`oc get route keycloak --template={{.spec.host}}`/auth \
     -DSSO_CLIENT_ID=openfact-online-platform \
     -DSSO_CREDENTIALS_SECRET=b7873f80-6ad7-44a5-97cb-bd210789eb41 \
+    -DHB_AUTO=create-drop \
+    -DHB_SHOW_SQL=true \
+    -DHB_FORMAT_SQL=true \
     -DHB_SECOND_LEVEL_CACHE=true \
     -DES_SERVER_URL=http://elasticsearch:9200 \
     -DES_USERNAME='' \
