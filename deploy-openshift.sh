9e90cb53111aa6cc76ff91a2cbdb304e9cbc35f5
Carlos Feria <carlosthe19916@gmail.com>
2017-11-09 10:01:21 -0500

    OFS-19: change config map and secrets

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -10,6 +10,14 @@ mvn fabric8:deploy -DSSO_REALM=openfact \
     -DSSO_AUTH_SERVER_URL=http://`oc get route keycloak --template={{.spec.host}}`/auth \
     -DSSO_CLIENT_ID=openfact-online-platform \
     -DSSO_CREDENTIALS_SECRET=b7873f80-6ad7-44a5-97cb-bd210789eb41 \
+    -DOPENFACT_SMTP_HOST=${SMTP_HOST} \
+    -DOPENFACT_SMTP_PORT=${SMTP_PORT} \
+    -DOPENFACT_SMTP_FROM=${SMTP_FROM} \
+    -DOPENFACT_SMTP_SSL=${SMTP_SSL} \
+    -DOPENFACT_SMTP_STARTTLS=${SMTP_STARTTLS} \
+    -DOPENFACT_SMTP_AUTH=${SMTP_AUTH}\
+    -DOPENFACT_SMTP_USER=$(echo -n ${SMTP_USER} | base64) \
+    -DOPENFACT_SMTP_PASSWORD=$(echo -n ${SMTP_PASSWORD} | base64) \
     -Popenshift \
     -DskipTests=true \
     -Dfabric8.debug.enabled=true
