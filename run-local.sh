483b5092f0eb122a7e91d8b36adf252e55beba19
Carlos Feria <carlosthe19916@gmail.com>
2018-03-20 17:53:13 -0500

    fix(rm): remove unused documents

--- run-local.sh
+++ run-local.sh
@@ -2,4 +2,6 @@
 
 rm -rf org.clarksnut.*
 mvn clean package -DskipTests
-java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 target/clarksnut-*-swarm.jar -Dsso.auth.server.url="http://localhost:8081/auth" -Dswarm.project.stage=peru
+java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 target/clarksnut-*-swarm.jar \
+-Dsso.auth.server.url="http://localhost:8081/auth" \
+-Dswarm.project.stage=peru
